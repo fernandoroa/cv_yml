7110e307645b5135b65a3147c478a428c3d2f593
Fernando Roa <froao@unal.edu.co>
2024-06-20 17:44:58 -0500

    refactor: split functions file

---
+++ R/process_yaml.R
@@ -0,0 +1,91 @@
+read_yml_and_check_levels <- function(yaml_file) {
+  yml_list <- classify_yaml_structure(yaml_file)
+
+  if (attr(yml_list, "yaml_type") == "one_level") {
+    yml_list <- map(yml_list, replace_system_vars)
+    attr(yml_list, "yaml_type") <- "one_level"
+  } else if (attr(yml_list, "yaml_type") == "multi_level") {
+    has_year <- map_lgl(yml_list, ~ "year" %in% names(.x))
+    if (any(has_year)) {
+      sublists_with_year <- yml_list[has_year]
+      sublists_without_year <- yml_list[!has_year]
+      sorted_indices <- order(
+        map_int(sublists_with_year, ~ extract_first_year(.x$year[[1]])),
+        decreasing = TRUE
+      )
+      sorted_sublists_with_year <- sublists_with_year[sorted_indices]
+      sorted_list <- c(sorted_sublists_with_year, sublists_without_year)
+      yml_list <- modify_nested_list(sorted_list)
+      attr(yml_list, "yaml_type") <- "multi_level"
+      return(yml_list)
+    }
+  }
+  yml_list
+}
+
+extract_first_year <- function(year_str) {
+  as.integer(str_extract(year_str, "^\\d{4}"))
+}
+
+classify_yaml_structure <- function(yaml_file) {
+  data <- yaml::yaml.load_file(yaml_file)
+
+  if (!is.list(data)) {
+    return(NULL)
+  }
+
+  # Check if 'data' is a one-level structure
+  is_one_level <- all(map_lgl(data, ~ is.list(.) && !any(names(.) == "id")))
+
+  # Check if 'data' is a multi-level structure (- id: ...)
+  is_multi_level <- all(map_lgl(data, ~ is.list(.) && "id" %in% names(.)))
+
+  if (is_one_level) {
+    attr(data, "yaml_type") <- "one_level"
+  } else if (is_multi_level) {
+    attr(data, "yaml_type") <- "multi_level"
+  } else {
+    attr(data, "yaml_type") <- "Unknown structure"
+  }
+
+  return(data)
+}
+
+# Function to apply changes to nested list and return the modified list
+modify_nested_list <- function(nested_list) {
+  modified_list <- map(nested_list, function(lst) {
+    param_keys <- find_params_keys(lst)
+    result_list <- lst
+    for (idx_key in seq_along(param_keys)) {
+      result_list <- modify(result_list, add_key_to_sublist,
+        key = names(param_keys[idx_key]),
+        value = param_keys[[idx_key]]
+      )
+    }
+    return(result_list)
+  })
+  return(modified_list)
+}
+
+find_params_keys <- function(lst) {
+  params_keys <- names(lst)[grepl("^params", names(lst))]
+  params_list <- lst[params_keys]
+  return(params_list)
+}
+
+# Recursive function to propagate 'params' keys to sublists
+add_key_to_sublist <- function(lst, key, value) {
+  if (is.list(lst) && is.null(lst[[key]])) {
+    lst[[key]] <- value
+  }
+  return(lst)
+}
+
+# Function to replace "system_var" with its value from environment variables
+replace_system_vars <- function(sublist) {
+  if (!is.null(sublist$system_var)) {
+    sublist$value <- Sys.getenv(sublist$system_var, unset = NA)
+    sublist$system_var <- NULL
+  }
+  return(sublist)
+}
