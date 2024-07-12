723c5c798a930c53e58b7d45a3e805a96a8adf7b
Fernando Roa <froao@unal.edu.co>
2024-07-12 15:45:44 -0500

    docs: add dependencies

--- dependencies.R
+++ dependencies.R
@@ -1,2 +1,8 @@
 # This file allows packrat (used by rsconnect during deployment) to pick up dependencies.
 library(rhino)
+library(purrr)
+library(dplyr)
+library(yaml)
+library(shiny)
+library(stringr)
+library(bib2df)
